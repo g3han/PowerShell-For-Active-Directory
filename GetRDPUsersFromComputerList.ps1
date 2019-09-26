f03e5adf06d24cdb1e8ec21fd82c385010b436c7
g3han <guchancelik@gmail.com>
2019-09-26 12:06:01 +0300

    Add files via upload

---
+++ GetRDPUsersFromComputerList.ps1
@@ -0,0 +1,18 @@
+﻿$groups = "Administrators";
+$servers = import-csv "c:\temp\computerlist.csv"
+foreach ($server in $servers) {
+    $servername = $server.name;
+    if((test-connection -computername $servername -count 1 -quiet)) {
+        foreach ($group in $groups) {
+            $localgroup = [ADSI]"WinNT://$servername/$group";
+            $members = @($localgroup.Invoke("Members"));
+            foreach ($member in $members) {
+                $memberName = $member.GetType().Invokemember("Name","GetProperty",$null,$member,$null);
+                $memberType = $member.GetType().Invokemember("Class","GetProperty",$null,$member,$null);
+                $outstring = $servername + "," + $group + "," + $membername + "," + $membertype;
+                write-host $outstring;
+                $outstring >> c:\temp\users_in_administrator_groups.csv;
+            }
+        }
+    }
+}
